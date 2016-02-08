e23111e07cdc4af58f906b7a7619fb4e07e7b2e8
Jacob Dubose <jdubose@IT405.mr.preble.com>
2016-02-08 14:58:29 -0500

    Mass update
    stop breaking my repos, github

---
+++ Roboter Jaeger Omega/Assets/GunMaster.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunMaster : MonoBehaviour {
+
+    [SerializeField] GameObject Weapon1;
+    [SerializeField] GameObject Weapon2;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Hello");
+            Weapon1.SetActive(true);
+            Weapon1.GetComponent<GunController>().enabled = true;
+
+            Weapon2.SetActive(false);
+            Weapon2.GetComponent<GunController>().enabled = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Goodbye");
+            Weapon1.SetActive(false);
+            Weapon1.GetComponent<GunController>().enabled = false;
+
+            Weapon2.SetActive(true);
+            Weapon2.GetComponent<GunController>().enabled = true;
+        }
+    }
+}
